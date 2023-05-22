b2cba01b09eca6c2e4c4d0691769bca5e61c080c
TranSyHuy <syhuy01072002@gmail.com>
2023-05-22 23:29:46 +0700

    [GM007]-Imple Page Home

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Trinh Khung</Text>
+      <Text>Trinh Rang Su</Text>
       <StatusBar style="auto" />
     </View>
   );
