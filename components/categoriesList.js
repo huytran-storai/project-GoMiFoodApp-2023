7300104bd03612a71222ea41612fef4708f0e20e
TranSyHuy <syhuy01072002@gmail.com>
2023-06-12 01:06:58 +0700

    [GM016] Fix code feature add to cart

--- components/categoriesList.js
+++ components/categoriesList.js
@@ -3,16 +3,20 @@ import React from 'react'
 import { TouchableOpacity } from "react-native";
 import { themeColors } from "../theme";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectCart } from "../slice/cartSlice";
 
-export default function categoriesList(){
+export default function categoriesList() {
     const navigation = useNavigation();
+    const cartItems = useSelector(selectCart);
+    if (!cartItems.length) return;
     return (
         <View className="absolute bottom-5 w-full z-50">
             <TouchableOpacity
-            onPress={() => navigation.navigate('Cart')}
-            style={{backgroundColor: themeColors.bgColor(1)}}
-            className="flex-row justify-between items-center mx-5 rounded-full p-4 py-3 shadow-lg">
-                <View className="p-2 px-4 rounded-full" style={{backgroundColor: 'rgba(255,255,255,0.3)'}}>
+                onPress={() => navigation.navigate('Cart')}
+                style={{ backgroundColor: themeColors.bgColor(1) }}
+                className="flex-row justify-between items-center mx-5 rounded-full p-4 py-3 shadow-lg">
+                <View className="p-2 px-4 rounded-full" style={{ backgroundColor: 'rgba(255,255,255,0.3)' }}>
                     <Text className="font-extrabold text-white text-lg">
                         3
                     </Text>
--- components/dishRow.js
+++ components/dishRow.js
@@ -1,15 +1,27 @@
 import { View, Text } from "react-native";
-import React from 'react' 
+import React from 'react'
 import { Image, TouchableOpacity } from "react-native";
 import { themeColors } from "../theme";
 import * as Icon from "react-native-feather";
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart, selectCartById } from "../slice/cartSlice";
+export default function DishRow({ item }) {
+    const dispatch = useDispatch()
 
-export default function DishRow({item}){
+    const handleDecrease = () => {
+        dispatch(removeFromCart({ id: item.id }))
+    }
+    const handleIncrease = () => {
+        dispatch(addToCart({ ...item }))
+    }
+
+    const totalItems = useSelector(state => selectCartById(state, item.id))
+
     return (
         <View className="flex-row items-center bg-white p-3 rounded-3xl shadow-2xl mb-3 mx-2">
-            <Image className="rounded-3xl" style={{height:100 , width:100 }}
-            source={item.image} />
-            
+            <Image className="rounded-3xl" style={{ height: 100, width: 100 }}
+                source={item.image} />
+
             <View className="flex flex-1 space-y-3">
                 <View className="pl-3">
                     <Text className="text-xl">{item.name}</Text>
@@ -21,17 +33,21 @@ export default function DishRow({item}){
                     </Text>
                     <View className="flex-row items-center">
                         <TouchableOpacity
+                            onPress={handleDecrease}
                             className="p-1 rounded-full"
-                            style={{backgroundColor: themeColors.bgColor(1)}}
+                            style={{
+                                backgroundColor: totalItems.length ? themeColors.bgColor(1) : themeColors.bgColor(0)
+                            }}
                         >
                             <Icon.Minus strokeWidth={2} height={20} width={20} stroke={'white'} />
                         </TouchableOpacity>
                         <Text className="px-3">
-                            {2}
+                            {totalItems.length}
                         </Text>
                         <TouchableOpacity
+                            onPress={handleIncrease}
                             className="p-1 rounded-full"
-                            style={{backgroundColor: themeColors.bgColor(1)}}
+                            style={{ backgroundColor: themeColors.bgColor(1) }}
                         >
                             <Icon.Plus strokeWidth={2} height={20} width={20} stroke={'white'} />
                         </TouchableOpacity>
