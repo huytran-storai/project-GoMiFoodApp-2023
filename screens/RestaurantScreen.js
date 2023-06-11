da5e555c3eda710b3957cea4495e9af4665e0e5e
TranSyHuy <syhuy01072002@gmail.com>
2023-06-12 00:27:35 +0700

    [GM-016] - Feature Add Cart

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -17,7 +17,7 @@ export default function RestaurantScreen() {
         if(item && item.id){
             dispatch(setRestaurant({...item}))
         }
-    })
+    },[])
     return (
         <View>
             <CartIconcopy />
