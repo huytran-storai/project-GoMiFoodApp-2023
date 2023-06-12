7ebb31347b9dd1f80920fe5f077ad4422a019efc
AzureAD\HuyTran <huy.t@stor.ai>
2023-06-12 18:19:13 +0700

    [GM018] UI Signup Login Page Demo

--- navigation.js
+++ navigation.js
@@ -13,23 +13,28 @@ import Intro2 from './screens/Intro2';
 import Intro3 from './screens/Intro3';
 import Intro4 from './screens/Intro4';
 import Intro5 from './screens/Intro5';
-
-export default function Navigation(){
+import WelcomeScreen from './screens/WelcomeScreen'
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
+export default function Navigation() {
     return (
         <NavigationContainer >
             <Stack.Navigator initialRouteName="Intro" screenOptions={{
                 headerShown: false
             }}>
-            <Stack.Screen name="Intro" component={Intro} />
-            <Stack.Screen name="Intro2" component={Intro2} />
-            <Stack.Screen name="Intro3" component={Intro3} />
-            <Stack.Screen name="Intro4" component={Intro4} />
-            <Stack.Screen name="Intro5" component={Intro5} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-            <Stack.Screen name="Cart" options={{presentation: 'modal'}} component={CartScreen} />
-            <Stack.Screen name="OrderPrepairing" options={{presentation: 'fullScreenModal'}} component={OrderPrepairing} />
-            <Stack.Screen name="Delivery" options={{presentation: 'fullScreenModal'}} component={DeliveryScreen} />
+                <Stack.Screen name="Welcome" component={WelcomeScreen} />
+                <Stack.Screen name="Login" component={LoginScreen}/>
+                <Stack.Screen name='SignUp' component={SignUpScreen}/>
+                <Stack.Screen name="Intro" component={Intro} />
+                <Stack.Screen name="Intro2" component={Intro2} />
+                <Stack.Screen name="Intro3" component={Intro3} />
+                <Stack.Screen name="Intro4" component={Intro4} />
+                <Stack.Screen name="Intro5" component={Intro5} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+                <Stack.Screen name="Cart" options={{ presentation: 'modal' }} component={CartScreen} />
+                <Stack.Screen name="OrderPrepairing" options={{ presentation: 'fullScreenModal' }} component={OrderPrepairing} />
+                <Stack.Screen name="Delivery" options={{ presentation: 'fullScreenModal' }} component={DeliveryScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
--- theme/index.js
+++ theme/index.js
@@ -1,4 +1,5 @@
 export const themeColors = {
     text: '#f97316',
-    bgColor: opacity=> `rgba(251, 146, 60, ${opacity})`
+    bgColor: opacity=> `rgba(251, 146, 60, ${opacity})`,
+    bg: '#877dfa',
 }
