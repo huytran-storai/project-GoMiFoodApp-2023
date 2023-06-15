37134d3860c9862427e5efc628da507b38eda4b6
HuyTran <huy.t@stor.ai>
2023-06-15 22:20:49 +0700

    [GM007] Update object for intro page

--- screens/TutorialScreen.js
+++ screens/TutorialScreen.js
@@ -8,15 +8,42 @@ const TutorialScreen = ({ navigation }) => {
     const goToLogIn = () => {
         navigation.navigate('Login');
     }
+    const tutorial = {
+        intro1:{
+            title:"About Our App",
+            title2:'Introducing',
+            desc:'We are sure you will be satisfied with our service',
+            desc2:'We are sure you will be satisfied with our service'
+        },
+        intro2:{
+            title:"About Our App",
+            title2:'Create Account',
+            desc:'First you must create an account',
+            desc2:'to become our member'
+        },
+        intro3:{
+            title:"About Our App",
+            title2:'Choose Food',
+            desc:'After create account, you can choose the food',
+            desc2:'you want to your cart'
+        },
+        intro4:{
+            title:"About Our App",
+            title2:'Delivery',
+            desc:'Finally we will deliver your order',
+            desc2:'as fast as possible'
+        }
+        
+    }
     return (
         <Swiper showsButtons loop={false}>
             <SafeAreaView style={{ width, height, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: '#000000', top: 73, left: 0, opacity: 0.6, position: 'absolute', width, height, fontSize: 18, fontWeight: 700, textAlign: 'center', lineHeight: 22 }}>About Our App</Text>
+                <Text style={{ color: '#000000', top: 73, left: 0, opacity: 0.6, position: 'absolute', width, height, fontSize: 18, fontWeight: 700, textAlign: 'center', lineHeight: 22 }}></Text>
                 <Text style={{ color: '#000000', top: 101, left: 0, position: 'absolute', width, height, fontSize: 20, fontWeight: 600, textAlign: 'center', lineHeight: 24 }}>Introducing</Text>
                 <Image source={require('../assets/images/logo.png')} style={{ width: 103, height: 89, position: 'absolute', left: 135, top: 125 }} />
                 <Image source={require('../assets/images/intro1.png')} style={{ width: '100%', height: '60%', resizeMode: 'contain', top: 220, position: 'absolute', borderRadius: 10 }} />
                 <Text style={{ color: '#000000', top: 690, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>We provide food delivery service</Text>
-                <Text style={{ color: '#000000', top: 702, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}> We are sure you will be satisfied with our service</Text>
+                <Text style={{ color: '#000000', top: 702, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>We are sure you will be satisfied with our service</Text>
                 <Text style={{ color: '#000000', top: 714, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>You can order the food on our app</Text>
                 <View style={{ position: 'absolute', bottom: 70, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
                     <TouchableOpacity onPress={goToLogIn} style={{ width: 120, height: 50, borderRadius: 30, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
