689b885d13440f6af680b102790a0de778bb6d6d
TranSyHuy <syhuy01072002@gmail.com>
2023-06-19 08:12:32 +0700

    [GM007] Color not the same mockup

--- navigation.js
+++ navigation.js
@@ -22,10 +22,6 @@ export default function Navigation() {
                 <Stack.Screen name='SignUp' component={SignUpScreen}/>
                 <Stack.Screen name="Intro" component={Intro} />
                 <Stack.Screen name="Tutorial" component={TutorialScreen}/>
-                {/* <Stack.Screen name="Intro2" component={Intro2} />
-                <Stack.Screen name="Intro3" component={Intro3} />
-                <Stack.Screen name="Intro4" component={Intro4} />
-                <Stack.Screen name="Intro5" component={Intro5} /> */}
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Restaurant" component={RestaurantScreen} />
                 <Stack.Screen name="Cart" options={{ presentation: 'modal' }} component={CartScreen} />
--- theme/index.js
+++ theme/index.js
@@ -1,5 +1,5 @@
 export const themeColors = {
     text: '#429f9e',
-    bgColor: opacity=> `rgba(66, 170, 245, ${opacity})`,
-    bg: '#877dfa',
+    bgColor: opacity=> `rgba(66, 159, 158, ${opacity})`,
+    bg: '#429f9e',
 }
