aee3499dbdf5197f61bdc3298c66fd19563765f3
TranSyHuy <syhuy01072002@gmail.com>
2023-06-19 09:38:46 +0700

    fomart code

--- components/categories.js
+++ components/categories.js
@@ -5,42 +5,42 @@ import { themeColors } from '../theme';
 //import { getCategories } from '../api'
 
 export default function Categories() {
- 
+
   const [activeCategory, setActiveCategory] = useState(null);
 
-   return (
+  return (
     <View className="mt-4">
       <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          className="overflow-visible"
-          contentContainerStyle={{
-            paddingHorizontal: 15
-          }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="overflow-visible"
+        contentContainerStyle={{
+          paddingHorizontal: 15
+        }}
       >
-{
-            categories.map((category)=>{
-              <Categories key={category.id} name={category.name} />
-              let isActive = category._id==activeCategory;
-              let btnClass = isActive? ' bg-gray-600': ' bg-gray-200';
-              let textClass = isActive? ' font-semibold text-gray-800': ' text-gray-500';
-              return(
-                <View key={category._id} className="flex justify-center items-center mr-6">
-                  <TouchableOpacity 
-                    onPress={()=> setActiveCategory(category._id)} 
-                    className={"p-1 rounded-full shadow"+ btnClass}>
-                    <Image style={{width: 45, height: 45}} 
-                    source={category.image} 
-                    />
-                  </TouchableOpacity>
-                  <Text className={"text-sm "+textClass}>{category.name}</Text>
-                </View> 
-              )
-            })
-          }
-        
+        {
+          categories.map((category, index) => {
+            <Categories key={index} name={category.name} />
+            let isActive = category._id == activeCategory;
+            let btnClass = isActive ? ' bg-gray-600' : ' bg-gray-200';
+            let textClass = isActive ? ' font-semibold text-gray-800' : ' text-gray-500';
+            return (
+              <View key={category._id} className="flex justify-center items-center mr-6">
+                <TouchableOpacity
+                  onPress={() => setActiveCategory(category._id)}
+                  className={"p-1 rounded-full shadow" + btnClass}>
+                  <Image style={{ width: 45, height: 45 }}
+                    source={category.image}
+                  />
+                </TouchableOpacity>
+                <Text className={"text-sm " + textClass}>{category.name}</Text>
+              </View>
+            )
+          })
+        }
+
       </ScrollView>
     </View>
-    
+
   )
 }
