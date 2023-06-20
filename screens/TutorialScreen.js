d6dc17d16bc10415b9b04a0b1aaac233191e5296
quanganh <quanganhdinh156@gmail.com>
2023-06-20 13:31:13 +0700

    "[GM234] - Change picture No.2 and change type CSS"

--- screens/TutorialScreen.js
+++ screens/TutorialScreen.js
@@ -13,81 +13,113 @@ const TutorialScreen = ({ navigation }) => {
             title:"About Our App",
             title2:'Introducing',
             desc:'We are sure you will be satisfied with our service',
-            desc2:'We are sure you will be satisfied with our service'
+            desc2:'We are sure you will be satisfied with our service',
+            image1: "../assets/images/logo.png",
+            image2: "../assets/images/intro1.png",
         },
         intro2:{
             title:"About Our App",
             title2:'Create Account',
             desc:'First you must create an account',
-            desc2:'to become our member'
+            desc2:'to become our member',
+            image1: "../assets/images/logo.png",
+            image2: "../assets/images/loginandsignup.png",
         },
         intro3:{
             title:"About Our App",
             title2:'Choose Food',
             desc:'After create account, you can choose the food',
-            desc2:'you want to your cart'
+            desc2:'you want to your cart',
+            image1: "../assets/images/logo.png",
+            image2: "../assets/images/intro3.png",
         },
         intro4:{
             title:"About Our App",
             title2:'Delivery',
             desc:'Finally we will deliver your order',
-            desc2:'as fast as possible'
+            desc2:'as fast as possible',
+            image1: "../assets/images/logo.png",
+            image2: "../assets/images/intro4.png",
         }
         
     }
     return (
         <Swiper showsButtons loop={false}>
-            <SafeAreaView style={{ width, height, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: '#000000', top: 73, left: 0, opacity: 0.6, position: 'absolute', width, height, fontSize: 18, fontWeight: 700, textAlign: 'center', lineHeight: 22 }}></Text>
-                <Text style={{ color: '#000000', top: 101, left: 0, position: 'absolute', width, height, fontSize: 20, fontWeight: 600, textAlign: 'center', lineHeight: 24 }}>Introducing</Text>
-                <Image source={require('../assets/images/logo.png')} style={{ width: 103, height: 89, position: 'absolute', left: 135, top: 125 }} />
-                <Image source={require('../assets/images/intro1.png')} style={{ width: '100%', height: '60%', resizeMode: 'contain', top: 220, position: 'absolute', borderRadius: 10 }} />
-                <Text style={{ color: '#000000', top: 690, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>We provide food delivery service</Text>
-                <Text style={{ color: '#000000', top: 702, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>We are sure you will be satisfied with our service</Text>
-                <Text style={{ color: '#000000', top: 714, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>You can order the food on our app</Text>
-                <View style={{ position: 'absolute', bottom: 70, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
-                    <TouchableOpacity onPress={goToLogIn} style={{ width: 120, height: 50, borderRadius: 30, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
+            <SafeAreaView className="bg-white w-full h-full justify-center items-center">
+                <View className="flex-1 items-center absolute w-full h-full">
+                    <Text className="text-gray-500 mt-3 left-1 font-bold text-2xl text-center leading-6">About Our App</Text>
+                    <Text className="text-black text-center left-0.5 font-semibold text-xl leading-6">Introducing</Text>
+                </View>
+                <View className="flex-1 mt-40 mr-32 mb-5 justify-center items-center ">
+                    <Image source={require('../assets/images/logo.png')} className="mt-0" style={{width: '65%', height: '33%', position: 'absolute', top: '-43%', left: '-11.5%'}} />
+                    <Image source={require('../assets/images/intro1.png')} className="mt-4" style={{width: '150%', height: '170%', position: 'absolute',top: '-16%', resizeMode: 'contain', left: '-50.5%', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 3 }}/>
+                </View>
+                <View className="flex-1 justify-center mt-9 top-0 mb-1">
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >We provide food delivery service.</Text>
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >We are sure you will be satisfied with our service</Text>
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >You can order the food on our app</Text>
+                </View>
+                <View className="mt-1" style={{ position: 'absolute', bottom: 42, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
+                    <TouchableOpacity onPress={goToLogIn} style={{ width: 100, height: 40, borderRadius: 30, shadowColor: '#000',shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.3, shadowRadius: 3, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
                         <Text style={{ color: '#429F9E', fontSize: 14, fontWeight: 700, textAlign: 'center' }}>{"Login Now!"}</Text>
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
-            <SafeAreaView style={{ width, height, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: '#000000', top: 73, left: 0, opacity: 0.6, position: 'absolute', width, height, fontSize: 18, fontWeight: 700, textAlign: 'center', lineHeight: 22 }}>About Our App</Text>
-                <Text style={{ color: '#000000', top: 101, left: 0, position: 'absolute', width, height, fontSize: 20, fontWeight: 600, textAlign: 'center', lineHeight: 24 }}>Create Account</Text>
-                <Image source={require('../assets/images/logo.png')} style={{ width: 103, height: 89, position: 'absolute', left: 135, top: 125 }} />
-                <Image source={require('../assets/images/intro2.1.png')} style={{ width: '100%', height: '55%', resizeMode: 'contain', top: 210, left: -70, position: 'absolute', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 3 }} />
-                <Image source={require('../assets/images/intro2.png')} style={{ width: '100%', height: '55%', resizeMode: 'contain', top: 240, left: 80, position: 'absolute', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 3 }} />
-                <Text style={{ color: '#000000', top: 690, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>First you must create an account</Text>
-                <Text style={{ color: '#000000', top: 704, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>to become our member</Text>
-                <View style={{ position: 'absolute', bottom: 70, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
-                    <TouchableOpacity onPress={goToLogIn} style={{ width: 120, height: 50, borderRadius: 30, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
+            <SafeAreaView className="bg-white w-full h-full justify-center items-center">
+                <View className="flex-1 items-center absolute w-full h-full">
+                    <Text className="text-gray-500 mt-3 left-1 font-bold text-2xl text-center leading-6">About Our App</Text>
+                    <Text className="text-black text-center left-0.5 font-semibold text-xl leading-6">Create Account</Text>
+                </View>
+                <View className="flex-1 mt-10 mr-36 left-10 justify-center ">
+                    <Image source={require('../assets/images/logo.png')} style={{ width: '75%', height: '28%', position: 'absolute', left: '-28%', top: '7%' }} />
+                    <Image source={require('../assets/images/loginandsignup.png')} className="mt-4" style={{ width: '130%', height: '125%', resizeMode: 'contain',justifyContent: 'center', top: '30%',left: '-52.5%', position: 'absolute'}} />
+                {/* <Image source={require('../assets/images/intro2.png')} style={{ width: '100%', height: '55%', resizeMode: 'contain', top: 240, left: 80, position: 'absolute', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 3 }} /> */}
+                </View>
+                <View className="flex-1 justify-center mt-5 top-9">
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >First you must create an account</Text>
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >to become our member</Text>
+                </View>
+                <View style={{ position: 'absolute', bottom: 42, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
+                    <TouchableOpacity onPress={goToLogIn} style={{ width: 100, height: 40, borderRadius: 30, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.3, shadowRadius: 3,  backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
                         <Text style={{ color: '#429F9E', fontSize: 14, fontWeight: 700, textAlign: 'center' }}>{"Login Now!"}</Text>
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
-            <SafeAreaView style={{ width, height, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: '#000000', top: 73, left: 0, opacity: 0.6, position: 'absolute', width, height, fontSize: 18, fontWeight: 700, textAlign: 'center', lineHeight: 22 }}>About Our App</Text>
-                <Text style={{ color: '#000000', top: 101, left: 0, position: 'absolute', width, height, fontSize: 20, fontWeight: 600, textAlign: 'center', lineHeight: 24 }}>Choose Food</Text>
-                <Image source={require('../assets/images/logo.png')} style={{ width: 103, height: 89, position: 'absolute', left: 135, top: 125 }} />
-                <Image source={require('../assets/images/intro3.png')} style={{ width: '100%', height: '55%', resizeMode: 'contain', top: 210, left: -70, position: 'absolute', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 3 }} />
-                <Image source={require('../assets/images/intro3.1.png')} style={{ width: '100%', height: '55%', resizeMode: 'contain', top: 240, left: 80, position: 'absolute', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 3 }} />
-                <Text style={{ color: '#000000', top: 690, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>After create account, you can choose the food</Text>
-                <Text style={{ color: '#000000', top: 704, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>you want to your cart</Text>
-                <View style={{ position: 'absolute', bottom: 70, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
-                    <TouchableOpacity onPress={goToLogIn} style={{ width: 120, height: 50, borderRadius: 30, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
+            <SafeAreaView className="bg-white" style={{ width, height, justifyContent: 'center', alignItems: 'center' }}>
+                <View className="flex-1 items-center absolute w-full h-full">
+                    <Text className="text-gray-500 mt-3 left-1 font-bold text-2xl text-center leading-6">About Our App</Text>
+                    <Text className="text-black text-center left-0.5 font-semibold text-xl leading-6">Choose Food</Text>
+                </View>
+                <View className="flex-1 mt-10 mr-36 left-10 justify-center ">
+                    <Image source={require('../assets/images/logo.png')} style={{ width: '75%', height: '28%', position: 'absolute', left: '-28%', top: '7%' }} />
+                    <Image source={require('../assets/images/intro3.png')} className="mt-4" style={{ width: '130%', height: '125%', resizeMode: 'contain', top: '30%', left: '-49.5%', position: 'absolute'}} />
+                    {/* <Image source={require('../assets/images/intro3.1.png')} style={{ width: '100%', height: '55%', resizeMode: 'contain', top: 240, left: 80, position: 'absolute', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 3 }} /> */}
+                </View>
+                <View className="flex-1 justify-center mt-5 top-9">
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >After create an account, you can choose the food</Text>
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >you want to add to your cart</Text>
+                </View>
+                <View style={{ position: 'absolute', bottom: 42, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
+                    <TouchableOpacity onPress={goToLogIn} style={{ width: 100, height: 40, borderRadius: 30,shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.3, shadowRadius: 3, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
                         <Text style={{ color: '#429F9E', fontSize: 14, fontWeight: 700, textAlign: 'center' }}>{"Login Now!"}</Text>
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
-            <SafeAreaView style={{ width, height, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: '#000000', top: 73, left: 0, opacity: 0.6, position: 'absolute', width, height, fontSize: 18, fontWeight: 700, textAlign: 'center', lineHeight: 22 }}>About Our App</Text>
-                <Text style={{ color: '#000000', top: 101, left: 0, position: 'absolute', width, height, fontSize: 20, fontWeight: 600, textAlign: 'center', lineHeight: 24 }}>Delivery</Text>
-                <Image source={require('../assets/images/logo.png')} style={{ width: 103, height: 89, position: 'absolute', left: 135, top: 125 }} />
-                <Image source={require('../assets/images/intro4.png')} style={{ width: '100%', height: '55%', resizeMode: 'contain', top: 220, left: 5, position: 'absolute', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 3 }} />
-                <Text style={{ color: '#000000', top: 690, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>Finally we will deliver your order</Text>
-                <Text style={{ color: '#000000', top: 704, left: 0, position: 'absolute', width, height, fontSize: 11, fontWeight: 500, textAlign: 'center', lineHeight: 13, display: 'flex', }}>as fast as possible</Text>
-                <View style={{ position: 'absolute', bottom: 70, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
-                    <TouchableOpacity onPress={goToLogIn} style={{ width: 120, height: 50, borderRadius: 30, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
+            <SafeAreaView className="bg-white" style={{ width, height, justifyContent: 'center', alignItems: 'center' }}>
+            <View className="flex-1 items-center absolute w-full h-full">
+                    <Text className="text-gray-500 mt-3 left-1 font-bold text-2xl text-center leading-6">About Our App</Text>
+                    <Text className="text-black text-center left-0.5 font-semibold text-xl leading-6">Delivery</Text>
+                </View>
+                <View className="flex-1 mt-10 mr-36 left-10 justify-center ">
+                    <Image source={require('../assets/images/logo.png')} style={{ width: '75%', height: '28%', position: 'absolute', left: '-28%', top: '7%' }} />
+                    <Image source={require('../assets/images/intro4.png')} className="mt-4" style={{ width: '130%', height: '125%', resizeMode: 'contain', top: '30%', left: '-52.5%', position: 'absolute'}} />
+                </View>
+                <View className="flex-1 justify-center mt-5 top-9">
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >After create an account, you can choose the food</Text>
+                    <Text className=" text-black left-0 text-center text-sm font-medium leading-4" >you want to add to your cart</Text>
+                </View>
+                <View style={{ position: 'absolute', bottom: 42, flexDirection: 'row', justifyContent: 'center', width, paddingHorizontal: 20 }}>
+                    <TouchableOpacity onPress={goToLogIn} style={{ width: 100, height: 40, borderRadius: 30,shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.3, shadowRadius: 3, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
                         <Text style={{ color: '#429F9E', fontSize: 14, fontWeight: 700, textAlign: 'center' }}>{"Login Now!"}</Text>
                     </TouchableOpacity>
                 </View>
