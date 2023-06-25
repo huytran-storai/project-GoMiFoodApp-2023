24e7a9c12d7b6a5d7b0116b1367e7eeef7629623
TranSyHuy <syhuy01072002@gmail.com>
2023-06-26 01:59:06 +0700

    [GM-022]-Validation Form Login/Sigup

--- client/screens/LoginScreen.js
+++ client/screens/LoginScreen.js
@@ -1,60 +1,81 @@
 import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
 
 export default function LoginScreen() {
   const navigation = useNavigation();
+  const [emailLogin, setEmailLogin] = useState('');
+  const [passwordLogin, setPasswordLogin] = useState('');
+  const [emailErrorLogin, setEmailLoginError] = useState('');
+  const [passwordErrorLogin, setPasswordLoginError] = useState('');
+
   const goToHomeScreen = () => {
-    navigation.navigate('HomeTabs');
-  }
+    if (validateFields()) {
+      navigation.navigate('HomeTabs');
+    }
+  };
+
+  const validateFields = () => {
+    let isValid = true;
+
+    if (emailLogin.trim() === '') {
+      setEmailLoginError('Vui lòng nhập địa chỉ email');
+      isValid = false;
+    } else {
+      setEmailLoginError('');
+    }
+
+    if (passwordLogin.trim() === '') {
+      setPasswordLoginError('Vui lòng nhập mật khẩu');
+      isValid = false;
+    } else {
+      setPasswordLoginError('');
+    }
+
+    return isValid;
+  };
+
   return (
     <View className="flex-1 bg-white" style={{ backgroundColor: '#429F9E' }}>
-      <SafeAreaView className="flex ">
+      <SafeAreaView className="flex">
         <View className="flex-row justify-center">
-          <Image source={require("../assets/images/logo2.png")}
-            style={{ width: 220, height: 200 }} />
+          <Image source={require("../assets/images/logo2.png")} style={{ width: 220, height: 200 }} />
         </View>
       </SafeAreaView>
-      <View
-        style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
-        className="flex-1 bg-white px-8 pt-8">
+      <View style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }} className="flex-1 bg-white px-8 pt-8">
         <View className="form space-y-2">
           <Text className="text-gray-700 ml-4">Địa chỉ email</Text>
           <TextInput
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+            className="p-4 bg-gray-100 text-gray-700 rounded-2xl "
             placeholder="Địa chỉ email"
-            value=""
+            value={emailLogin}
+            onChangeText={setEmailLogin}
           />
+          {emailErrorLogin !== '' && <Text className="text-red-500 ml-4">{emailErrorLogin}</Text>}
           <Text className="text-gray-700 ml-4">Mật khẩu</Text>
           <TextInput
             className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
             secureTextEntry
             placeholder="Mật khẩu"
-            value=""
+            value={passwordLogin}
+            onChangeText={setPasswordLogin}
           />
+          {passwordErrorLogin !== '' && <Text className="text-red-500 ml-4">{passwordErrorLogin}</Text>}
           <TouchableOpacity className="flex items-end">
             <Text className="text-gray-700 mb-5">Quên mật khẩu?</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={goToHomeScreen}
-            className="py-3 bg-yellow-400 rounded-xl">
-            <Text
-              className="text-xl font-bold text-center text-gray-700"
-            >
-              Đăng nhập
-            </Text>
+          <TouchableOpacity onPress={goToHomeScreen} className="py-3 bg-yellow-400 rounded-xl">
+            <Text className="text-xl font-bold text-center text-gray-700">Đăng nhập</Text>
           </TouchableOpacity>
         </View>
         <View className="flex-row justify-center space-x-12 mt-4">
-        <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
-                <Image source={require('../assets/google.png')} 
-                    className="w-10 h-10" />
-            </TouchableOpacity>
+          <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+            <Image source={require('../assets/google.png')} className="w-10 h-10" />
+          </TouchableOpacity>
         </View>
         <View className="flex-row justify-center mt-7">
-          <Text className="text-gray-500 font-semibold">
-            Bạn chưa có tài khoản?
-          </Text>
+          <Text className="text-gray-500 font-semibold">Bạn chưa có tài khoản?</Text>
           <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
             <Text className="font-semibold text-yellow-500">Đăng kí</Text>
           </TouchableOpacity>
--- client/screens/SignUpScreen.js
+++ client/screens/SignUpScreen.js
@@ -1,79 +1,130 @@
 import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { themeColors } from '../theme'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {ArrowLeftIcon} from 'react-native-heroicons/solid';
+import { ArrowLeftIcon } from 'react-native-heroicons/solid';
 import { useNavigation } from '@react-navigation/native';
 
 // subscribe for more videos like this :)
 export default function SignUpScreen() {
     const navigation = useNavigation();
-  return (
-    <View className="flex-1 bg-white" style={{backgroundColor:'#429F9E'}}>
-      <SafeAreaView className="flex">
-        <View className="flex-row justify-start">
-            <TouchableOpacity 
-                onPress={()=> navigation.goBack()}
-                className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4"
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [fullNameError, setFullNameError] = useState('');
+    const [phoneNumberError, setPhoneNumberError] = useState('');
+    const goToHomeScreen = () => {
+        if (validateFields()) {
+            navigation.navigate('HomeTabs');
+        }
+    };
+    const validateFields = () => {
+        let isValid = true;
+        if (fullName.trim() === '') {
+            setFullNameError('Vui lòng nhập họ và tên');
+            isValid = false;
+        } else {
+            setFullNameError('');
+        }
+
+        if (phoneNumber.trim() === '') {
+            setPhoneNumberError('Vui lòng nhập số điện thoại');
+            isValid = false;
+        } else {
+            setPhoneNumberError('');
+        }
+
+        if (email.trim() === '') {
+            setEmailError('Vui lòng nhập địa chỉ email');
+            isValid = false;
+        } else {
+            setEmailError('');
+        }
+
+        if (password.trim() === '') {
+            setPasswordError('Vui lòng nhập mật khẩu');
+            isValid = false;
+        } else {
+            setPasswordError('');
+        }
+
+        return isValid;
+    };
+    return (
+        <View className="flex-1 bg-white" style={{ backgroundColor: '#429F9E' }}>
+            <SafeAreaView className="flex">
+                <View className="flex-row justify-start">
+                    <TouchableOpacity
+                        onPress={() => navigation.goBack()}
+                        className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4"
+                    >
+                        <ArrowLeftIcon size="20" color="black" />
+                    </TouchableOpacity>
+                </View>
+                <View className="flex-row justify-center">
+                </View>
+            </SafeAreaView>
+            <View className="flex-1 bg-white px-8 pt-8"
+                style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
             >
-                <ArrowLeftIcon size="20" color="black" />
-            </TouchableOpacity>
+                <View className="form space-y-2">
+                    <Text className="text-gray-700 ml-4">Họ và tên</Text>
+                    <TextInput
+                        className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
+                        value={fullName}
+                        onChangeText={setFullName}
+                        placeholder='Họ và tên'
+                    />
+                    {fullNameError !== '' && <Text className="text-red-500 ml-4">{fullNameError}</Text>}
+                    <Text className="text-gray-700 ml-4">Địa chỉ email</Text>
+                    <TextInput
+                        className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
+                        value={email}
+                        onChangeText={setEmail}
+                        placeholder='Địa chỉ email'
+                    />
+                    {emailError !== '' && <Text className="text-red-500 ml-4">{emailError}</Text>}
+                    <Text className="text-gray-700 ml-4">Số điện thoại</Text>
+                    <TextInput
+                        className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
+                        value={phoneNumber}
+                        onChangeText={setPhoneNumber}
+                        placeholder='Số điện thoại'
+                    />
+                    {phoneNumberError !== '' && <Text className="text-red-500 ml-4">{phoneNumberError}</Text>}
+                    <Text className="text-gray-700 ml-4">Mật Khẩu</Text>
+                    <TextInput
+                        className="p-4 bg-gray-100 text-gray-700 rounded-2xl "
+                        secureTextEntry
+                        value={password}
+                        onChangeText={setPassword}
+                        placeholder='Mật Khẩu'
+                    />
+                    {passwordError !== '' && <Text className="text-red-500 ml-4">{passwordError}</Text>}
+                    <TouchableOpacity
+                        className="py-3 bg-yellow-400 rounded-xl" onPress={goToHomeScreen}
+                    >
+                        <Text className="font-xl font-bold text-center text-gray-700">
+                            Đăng kí
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View className="flex-row justify-center space-x-12 mt-4">
+                    <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+                        <Image source={require('../assets/google.png')}
+                            className="w-10 h-10" />
+                    </TouchableOpacity>
+                </View>
+                <View className="flex-row justify-center mt-7">
+                    <Text className="text-gray-500 font-semibold">Bạn đã có tài khoản rồi?</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                        <Text className="font-semibold text-yellow-500"> Đăng nhập</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
-        <View className="flex-row justify-center">
-            {/* <Image source={require('../assets/images/signup.png')} 
-                style={{width: 165, height: 110}} /> */}
-        </View>
-      </SafeAreaView>
-      <View className="flex-1 bg-white px-8 pt-8"
-        style={{borderTopLeftRadius: 50, borderTopRightRadius: 50}}
-      >
-        <View className="form space-y-2">
-            <Text className="text-gray-700 ml-4">Họ và tên</Text>
-            <TextInput
-                className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-                value=""
-                placeholder='Họ và tên'
-            />
-            <Text className="text-gray-700 ml-4">Địa chỉ email</Text>
-            <TextInput
-                className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-                value=""
-                placeholder='Địa chỉ email'
-            />
-            <Text className="text-gray-700 ml-4">Số điện thoại</Text>
-            <TextInput
-                className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-                value=""
-                placeholder='Số điện thoại'
-            />
-            <Text className="text-gray-700 ml-4">Mật Khẩu</Text>
-            <TextInput
-                className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-7"
-                secureTextEntry
-                value=""
-                placeholder='Mật Khẩu'
-            />
-            <TouchableOpacity
-                className="py-3 bg-yellow-400 rounded-xl"
-            >
-                <Text className="font-xl font-bold text-center text-gray-700">
-                    Đăng kí
-                </Text>
-            </TouchableOpacity>
-        </View>
-        <View className="flex-row justify-center space-x-12 mt-4">
-            <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
-                <Image source={require('../assets/google.png')} 
-                    className="w-10 h-10" />
-            </TouchableOpacity>
-        </View>
-        <View className="flex-row justify-center mt-7">
-            <Text className="text-gray-500 font-semibold">Bạn đã có tài khoản rồi?</Text>
-            <TouchableOpacity onPress={()=> navigation.navigate('Login')}>
-                <Text className="font-semibold text-yellow-500"> Đăng nhập</Text>
-            </TouchableOpacity>
-        </View>
-      </View>
-    </View>
-  )
+    )
 }
