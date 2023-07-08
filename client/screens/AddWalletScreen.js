482c6938a60267546b005cccb6c3bff5619b7b59
quanganh <quanganhdinh156@gmail.com>
2023-07-08 21:49:06 +0700

    UPDATE NEW VALIDATION

--- client/screens/AddWalletScreen.js
+++ client/screens/AddWalletScreen.js
@@ -44,7 +44,11 @@ export default function AddWalletScreen() {
   const [text4, setText4] = useState('');
   const [text5, setText5] = useState('');
 
+  const [textError, setTextError] = useState('');
+  const [text2Error, setText2Error] = useState('');
+  const [text3Error, setText3Error] = useState('');
   const [text4Error, setText4Error] = useState('');
+  const [text5Error, setText5Error] = useState('');
 
   const handleChangeText = (inputText) => {
     setText(inputText);
@@ -73,6 +77,47 @@ export default function AddWalletScreen() {
             isValid = false;
         } else {
             setText4Error('');
+        };
+        //xac thuc firstname
+    const firstnameRegex = /^[a-zA-Z\s]+$/;
+        if (text.trim() === '') {
+            setTextError('Vui long nhap ten');
+            isValid = false;
+        } else if (!firstnameRegex.test(text.trim())) {
+            setTextError('Tên chỉ được chứa chữ');
+            isValid = false;
+        } else {
+            setTextError('');
+        };
+    const lastnameRegex = /^[a-zA-Z\s]+$/;
+        if (text2.trim() === '') {
+            setText2Error('Vui lòng nhập họ');
+            isValid = false;
+        } else if (!lastnameRegex.test(text2.trim())) {
+            setText2Error('Họ và tên chỉ được chứa chữ');
+            isValid = false;
+        } else {
+            setText2Error('');
+        };
+    const numberRegex = /^[a-zA-Z\s]+$/;
+        if (text3.trim() === '') {
+            setText3Error('Vui lòng nhập so the vao');
+            isValid = false;
+        } else if (!numberRegex.test(text3.trim())) {
+            setText3Error('So the khong duoc chua chu');
+            isValid = false;
+        } else {
+            setText3Error('');
+        };
+    const cvnRegex = /^\d{3}$/;
+        if (text5.trim() === '') {
+            setText5Error('Vui lòng so ma cvn');
+            isValid = false;
+        } else if (!cvnRegex.test(text5.trim())) {
+            setText5Error('CVN chi chua 3 so');
+            isValid = false;
+        } else {
+            setText5Error('');
         }
   }
   const navigation = useNavigation();
@@ -116,12 +161,14 @@ export default function AddWalletScreen() {
           className="w-[300px] h-[30px] mt-2 ml-1 bg-white text-black rounded-2xl mb-3 border border border border border-stone-500"
           value={text}
         />
+        {textError !== '' && <Text className="text-red-500 text-[10px]">{textError}</Text>}
         <Text className="text-black text-[13px] font-semibold mt-[3px] ml-3">Last Name</Text>
         <TextInput
           onChangeText={handleChangeText2}
           className="w-[300px] h-[30px] mt-2 ml-1 bg-white text-black rounded-2xl mb-3 border border border border border-stone-500"
           value={text2}
         />
+        {text2Error !== '' && <Text className="text-red-500 text-[10px]">{text2Error}</Text>}
         <Text className=" text-black mt-[25px] ml-2 text-[12.5px] font-semibold">Detail Payment</Text>
         <View className="w-[393px] h-[0px] mt-1 opacity-40 border border-black"></View>
         <View className="flex-row space-x-4 mt-[10px]">
@@ -156,7 +203,7 @@ export default function AddWalletScreen() {
         className="w-[300px] h-[30px] mt-[7px] ml-[6px] bg-white text-black rounded-2xl mb-3 border border border border border-stone-500"
         value={text3}
       />
-
+      {text3Error !== '' && <Text className="text-red-500 text-[10px]">{text3Error}</Text>}
       <View className="flex-row mt-[20px] space-x-[120px] ml-8">
         <Text className=" text-black text-[12px] font-normal">Expires month</Text>
         <Text className=" text-black text-[12px] font-normal">Expires years</Text>
@@ -196,8 +243,8 @@ export default function AddWalletScreen() {
           onChangeText={handleChangeText5}
           className="w-[100px] h-[30px] bg-white text-black text-center rounded-2xl border border border border border-stone-500"
           value={text5}>
-
         </TextInput>
+        {text5Error !== '' && <Text className="text-red-500 text-[10px]">{text5Error}</Text>}
       </View>
       <View className="justify-center items-center mt-[10px]">
         <TouchableOpacity onPress={handleSubmit} className="w-[135px] h-[48px] mt-[10px] bg-teal-500 rounded-2xl justify-center items-center">
