a7b95ccb64226ff88a6f396dfaebc6da865f2ac7
TranSyHuy <syhuy01072002@gmail.com>
2023-07-10 14:36:00 +0700

    Changed Icon Back

--- client/screens/MyWalletScreen.js
+++ client/screens/MyWalletScreen.js
@@ -1,8 +1,8 @@
 import { SafeAreaView,TouchableOpacity, View, Text, } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import DropDownPicker from 'react-native-dropdown-picker';
-
+import { ChevronRightIcon } from 'react-native-heroicons/solid';
+import { ChevronLeftIcon } from 'react-native-heroicons/solid';
 export default function MyWalletScreen() {
     const navigation = useNavigation();
     const goToProfileUserScreen = () => {
@@ -11,56 +11,18 @@ export default function MyWalletScreen() {
     const goToAddWalletScreen = () => {
         navigation.navigate('AddWallet');
     }
-    // const [open, setOpen] = useState(false);
-    // const [value, setValue] = useState(null);
-    // const [items, setItems] = useState([
-    //     { label: 'Thêm thẻ tín dụng vào GoMiWallet', value: 'option1' },
-    //     { label: 'Visa', value: 'option2' },
-    //     { label: 'Master', svalue: 'option3' },
-    //     { label: 'JCB', value: 'option4' }
-    // ]);
-
-    // useEffect(() => {
-    //     if (value === 'option1') {
-    //         navigation.navigate('AddWallet');
-    //     } else if (value === 'option2') {
-    //         navigation.navigate('Screen2');
-    //     }
-    // }, [value]);
-
-    // const handleItemSelect = (item) => {
-    //     setValue(item.value);
-    // }
 
     return (
         <SafeAreaView className="w-[393px] h-[852px] bg-white">
             <View className="flex-row space-x-[90px]">
-            <TouchableOpacity onPress={goToProfileUserScreen} className="w-[45px] h-[45px] ml-[13px] bg-teal-500 rounded-full justify-center items-center">
-                <Text className="text-[30px] font-large text-white font-bold rounded-full">{"<"}</Text>
+            <TouchableOpacity onPress={goToProfileUserScreen} className="w-[35px] h-[35px] ml-[13px] bg-teal-500 rounded-full justify-center items-center">
+            <ChevronLeftIcon style={{color:'white'}}></ChevronLeftIcon>
             </TouchableOpacity>
-            <Text className=" text-black text-[20px] font-extrabold">My Wallet</Text>
             </View>
-            
-            <View className="w-[393px] h-[49px] mt-[10px] bg-white border border border border border-stone-300 justify-center">
-                {/* <DropDownPicker
-                    placeholder="GoMiPay"
-                    open={open}
-                    value={value}
-                    items={items}
-                    setOpen={setOpen}
-                    setValue={setValue}
-                    setItems={setItems}
-                    onChangeItem={handleItemSelect}
-                    style={{ borderWidth: 0 }}
-                    dropDownContainerStyle={{ borderWidth: 0, left: 10 }}
-                    containerStyle={{ width: '100%', height: '100%' }}
-                    itemStyle={{
-                        justifyContent: 'flex-start'
-                    }}
-                /> */}
+            <View className="w-[393px] h-[49px] mt-[10px] bg-white border border-stone-300 justify-center">
             <TouchableOpacity onPress={goToAddWalletScreen} className=" flex-row space-x-[130px] ml-[10px] ">
-                <Text className="text-[15px] mt-[5px] font-large text-black font-bold">{"Thêm thẻ tín vào ví GoMiWallet"}</Text>
-                <Text className="text-[20px] font-large text-black font-bold">{">"}</Text>
+                <Text className="text-[15px] mt-[5px] font-large text-black font-bold">Thêm thẻ tín vào ví GoMiWallet</Text>
+                <ChevronRightIcon style={{color:'black'}}></ChevronRightIcon>
             </TouchableOpacity>
             </View>
         </SafeAreaView>
