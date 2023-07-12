8d39eefca1453cdf4328aa182d4830e6f9e6e0ec
Haitrinh <akashinguyen2001@gmail.com>
2023-07-12 10:32:51 +0700

    [GM-028-1] Fix contents Department screen

--- client/screens/DepartmentScreen.js
+++ client/screens/DepartmentScreen.js
@@ -6,14 +6,14 @@ import data from '../src/data'
 export default function DepartmentScreen() {
     const renderItem = ({ item, index }) => {
         return (
-
-            <View className='ml-2'>
-                <TouchableOpacity className="m-3 ">
+           
+            <View className='ml-2 '>
+                <TouchableOpacity className="m-3">
                     <View>
                         <ImageBackground imageStyle={{ borderTopLeftRadius: 10, borderTopRightRadius: 10 }} className=" w-40 h-40  " source={item.image} resizeMode="cover">
                         </ImageBackground>
                     </View>
-                    <View className=' bg-emerald-700 rounded-br-2xl rounded-bl-2xl'>
+                    <View className=' bg-teal-600 rounded-br-2xl rounded-bl-2xl'>
                         <Text className='text-center text-white font-black text-lg   '>{item.name}</Text>
                     </View>
                 </TouchableOpacity>
@@ -23,7 +23,7 @@ export default function DepartmentScreen() {
         )
     }
     return (
-        <SafeAreaView className="bg-white">
+        <SafeAreaView className="bg-white ">
             <Text className="text-[20px] bottom-[5px] font-large text-center font-bold rounded-full p-3">Danh Mục</Text>
             <View className="flex-row items-center space-x-2 px-4 pb-2 ">
                 <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
@@ -37,7 +37,10 @@ export default function DepartmentScreen() {
 
 
             </View>
-            <FlatList
+            <FlatList 
+            contentContainerStyle={{
+                paddingBottom: 100
+            }}
                 data={data}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
