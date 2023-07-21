a40c122192b2028cd9da934ab78d4af67d328397
TranSyHuy <syhuy01072002@gmail.com>
2023-07-21 17:14:58 +0700

    Fix scroll view

--- client/screens/RestaurantScreen.js
+++ client/screens/RestaurantScreen.js
@@ -23,46 +23,44 @@ export default function RestaurantScreen() {
         <View>
             <AddButtonCart />
             <StatusBar style='light' />
-
-            <ScrollView>
-                <View className="relative">
-                    <Image className="w-full h-72" source={{ uri: urlFor(item.image).url() }} />
-                    <TouchableOpacity
-                        onPress={() => navigation.goBack()}
-                        className="absolute top-14 left-4 bg-gray-50 p-2 rounded-full shadow">
-                        <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
-                    </TouchableOpacity>
-                </View>
-                <View
-                    style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
-                    className="bg-white -mt-12 pt-6"
-                >
-                    <View className="px-5">
-                        <Text className="text-3xl font-bold">{item.name}</Text>
-                        <View className="flex-row space-x-2 my-1">
-                            <View className="flex-row items-center space-x-1">
-                                <Image source={require('../assets/images/fullStar.png')} className="h-4 w-4" />
-                                <Text className="text-xs">
-                                    <Text className="text-green-700">{item.stars}</Text>
-                                    <Text className="text-gray-700">
-                                        ({item.reviews} review) . <Text className="font-semibold">{item?.type?.name}</Text>
-                                    </Text>
-                                </Text>
-                            </View>
-                        </View>
+            <View className="relative">
+                <Image className="w-full h-72" source={{ uri: urlFor(item.image).url() }} />
+                <TouchableOpacity
+                    onPress={() => navigation.goBack()}
+                    className="absolute top-14 left-4 bg-gray-50 p-2 rounded-full shadow">
+                    <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
+                </TouchableOpacity>
+            </View>
+            <View
+                style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
+                className="bg-white -mt-12 pt-6"
+            >
+                <View className="px-5">
+                    <Text className="text-3xl font-bold">{item.name}</Text>
+                    <View className="flex-row space-x-2 my-1">
                         <View className="flex-row items-center space-x-1">
-                            <Icon.MapPin color="gray" width="15" height="15" />
-                            <Text className="text-gray-700 text-xs">{item.address}</Text>
+                            <Image source={require('../assets/images/fullStar.png')} className="h-4 w-4" />
+                            <Text className="text-xs">
+                                <Text className="text-green-700">{item.stars}</Text>
+                                <Text className="text-gray-700">
+                                    ({item.reviews} review) . <Text className="font-semibold">{item?.type?.name}</Text>
+                                </Text>
+                            </Text>
                         </View>
                     </View>
+                    <View className="flex-row items-center space-x-1">
+                        <Icon.MapPin color="gray" width="15" height="15" />
+                        <Text className="text-gray-700 text-xs">{item.address}</Text>
+                    </View>
                 </View>
-                <View className="pb-36 bg-white">
-                    <Text className="px-4 py-4 text-2xl font-bold">Menu</Text>
-                    {/* dishes */}
-                    {
-                        item.dishes.map((dish, index) => <DishRow item={{ ...dish }} key={index} />)
-                    }
-                </View>
+            </View>
+            <Text className="px-4 py-4 text-2xl font-bold">Menu</Text>
+            <ScrollView className="pb-[100px]">
+            <View className=" bg-white pb-[500px]">
+                {
+                    item.dishes.map((dish, index) => <DishRow item={{ ...dish }} key={index} />)
+                }
+            </View>
             </ScrollView>
         </View>
     )
