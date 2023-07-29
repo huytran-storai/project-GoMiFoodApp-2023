99758834fbfc924d9d2eb3d880f913df7311913c
TranSyHuy <syhuy01072002@gmail.com>
2023-07-29 08:50:16 +0700

    Fix UI search bar

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -32,9 +32,9 @@ export default function HomeScreen({navigation}) {
                 <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
                 
                     <Icon.Search height="25" width="25" stroke="gray" />
-                    <TextInput placeholder='Restaurants...' className="ml-2 flex-1" keyboardType='default'/>
+                    <Text className="ml-2">.....</Text>
                     
-                    <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-gray-300">
+                    <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-[120px] border-l-gray-300">
                         <Icon.MapPin height="20" width="20" stroke="gray" />
                         <Text className="text-gray-600">Ho Chi Minh,HCM</Text>
                     </View>
